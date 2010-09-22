8fb37bee6d88aa51fc21fa27ddb106b72c06a66d
Jaakko Vallo <jaakko.vallo@iki.fi>
2010-09-22 18:15:25 +0300

    Namespaced code, added proper error handling, added sorting to result arrays

--- lib/resourceful.rb
+++ lib/resourceful.rb
@@ -1,42 +1,66 @@
-require 'rubygems'
-require 'pp'
+module Resourceful
 
-class Resourceful
+  E_NOT_ENOUGH_RESOURCES = "Not enough resources for task %s (%d required, %d available)"
 
-  E_NOT_ENOUGH_RESOURCES = "Not enough resources"
+  class Solver
+    attr_accessor :resources, :schedule
 
-  attr_accessor :resources, :schedule
+    def initialize(schedule=[], resources=[])
+      @schedule = schedule
+      @resources = resources
 
-  def initialize(schedule=[], resources=[])
-    @schedule = schedule
-    @resources = resources
-  end
-
-  def least_worked(task="", time=0)
-    valid_resources = @resources.find_all{ |res| res[:last_worked] != task }
-    res = valid_resources.min { |a,b| a[:time_worked] <=> b[:time_worked] }
-    res[:time_worked] += time
-    res[:last_worked] = task
-    res[:name]
-  end
-
-  def do_scheduling
-    result_work_schedule = []
-
-    # Sort tasks from most hours required to least hours required
-    @schedule.sort! { |a,b| b[:time] <=> a[:time] }
-
-    @schedule.each do |task|
-      if task[:resources] > @resources.length
-        result_work_schedule << { :task => task[:task], :resources => [E_NOT_ENOUGH_RESOURCES] }
-        next
-      else
-        workers = []
-        task[:resources].times { workers << least_worked(task[:task], task[:time]) }
-        result_work_schedule << { :task => task[:task], :resources => workers }
+      @resources.map do |res|
+        res[:time_worked] = 0
+        res[:last_worked] = ""
       end
     end
-    result_work_schedule
+
+    def least_worked(task="", time=0)
+      valid_resources = @resources.find_all{ |res| res[:last_worked] != task }
+      res = valid_resources.min { |a,b| a[:time_worked] <=> b[:time_worked] }
+
+      res[:time_worked] += time
+      res[:last_worked] = task
+      res[:name]
+    end
+
+    def do_scheduling
+      result_work_schedule = Array.new(@schedule.size, nil)
+
+      # Sort tasks from most hours required to least hours required
+      src_schedule = @schedule.sort { |a,b| b[:time] <=> a[:time] }
+
+      src_schedule.each do |task|
+        if task[:resources] > @resources.size
+          r_error = NotEnoughResourcesError.new(task[:task], task[:resources], @resources.size)
+          raise r_error, sprintf(E_NOT_ENOUGH_RESOURCES, task[:task], task[:resources], @resources.size) , caller
+        else
+          workers = []
+          task[:resources].times { workers << least_worked(task[:task], task[:time]) }
+          workers.sort!
+          result_work_schedule[original_position(task[:task])] = { :task => task[:task], :resources => workers }
+        end
+      end
+
+      result_work_schedule
+    end
+
+    def original_position(task)
+      @schedule.each_index do |i|
+        return i if @schedule[i][:task] == task
+      end
+    end
+
+  end
+
+  class NotEnoughResourcesError < StandardError
+    attr_reader :task, :resources_required, :resources_available
+
+    def initialize(failed_task, resources_required, resources_available)
+      @task = failed_task
+      @resources_required = resources_required
+      @resources_available = resources_available
+    end
   end
 
 end
