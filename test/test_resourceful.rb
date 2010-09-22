e666daab4a8c783f7c8d2d1eb131433e4f17380e
Jaakko Vallo <jaakko.vallo@iki.fi>
2010-09-22 18:15:53 +0300

    Adapted the tests to Contest. Cleaned and rearranged stuff.

--- test/test_resourceful.rb
+++ test/test_resourceful.rb
@@ -1,74 +1,79 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'yaml'
-require 'test/unit'
+require 'contest'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'resourceful')
 
 class ResourcefulTest < Test::Unit::TestCase
-  def setup
-    # A sequential schedule of needs
-    # Task name:Time required:Resources required
-    @schedule = [
-                { :task => "A", :time => 3, :resources => 1 },
-                { :task => "B", :time => 2, :resources => 2 },
-                { :task => "C", :time => 1, :resources => 3 }
-               ]
 
-    @resources = [
-                 { :name => "X", :time_worked => 0, :last_worked => "" },
-                 { :name => "Y", :time_worked => 0, :last_worked => "" },
-                 { :name => "Z", :time_worked => 0, :last_worked => "" }
-                ]
+  setup do
+    @schedule = [{ :task => "Restroom cleaning", :time => 1, :resources => 2 },
+                 { :task => "Flyer distribution", :time => 2, :resources => 3 },
+                 { :task => "Tour of the venue", :time => 3, :resources => 1 }]
 
-    @naive_results = [
-                      { :task => "A", :resources => ["X"] },
-                      { :task => "B", :resources => ["Y", "Z"] },
-                      { :task => "C", :resources => ["Y", "Z", "X"] }
-                     ]
-    @complex_naive_results = [
-                              { :task=>"A", :resources=>["X"] },
-                              { :task=>"B", :resources=>["Y", "Z"] },
-                              { :task=>"C", :resources=>["Y", "Z", "X"] },
-                              { :task=>"D", :resources=>["Y", "Z"] },
-                              { :task=>"E", :resources=>["X", "Y"] },
-                              { :task=>"F", :resources=>["X"] },
-                              { :task=>"G", :resources=>["Z", "X"] },
-                              { :task=>"H", :resources=>["Y"] },
-                              { :task=>"I", :resources=>["Y", "Z", "X"] }
-                             ]
+    @resources = [{ :name => "Alice" },
+                  { :name => "Bob" },
+                  { :name => "Eve" }]
 
-    @complex_schedule = YAML::load(open("complex_schedule.yml"))
-
-    @rful = Resourceful.new
-    @populated_rful = Resourceful.new(@schedule, @resources)
   end
 
-  def test_accept_resource_hash_data
-    @rful.resources = @resources
-    assert_equal @resources, @rful.resources
+  context "simple data acceptance" do
+    setup do
+      @rful = Resourceful::Solver.new
+    end
+
+    should "accept resource hash data" do
+      @rful.resources = @resources
+      assert_equal @resources, @rful.resources
+    end
+
+    should "accept schedule hash data" do
+      @rful.schedule = @schedule
+      assert_equal @schedule, @rful.schedule
+    end
+
+    should "accept schedule and resource hash data on creation" do
+      assert_nothing_raised { rful = Resourceful::Solver.new(@schedule, @resources) }
+    end
   end
 
-  def test_accept_schedule_hash_data
-    @rful.schedule = @schedule
-    assert_equal @schedule, @rful.schedule
-  end
+  context "scheduling runs" do
+    setup do
+      @populated_rful = Resourceful::Solver.new(@schedule, @resources)
+    end
 
-  def test_accept_schedule_and_resource_hash_data_on_creation
-    assert_nothing_raised { rful = Resourceful.new(@schedule, @resources) }
-  end
+    test "naive scheduling" do
+      @naive_results = [{ :task => "Restroom cleaning", :resources => ["Bob", "Eve"] },
+                        { :task => "Flyer distribution", :resources => ["Alice", "Bob", "Eve"] },
+                        { :task => "Tour of the venue", :resources => ["Alice"] }]
+      assert_equal @naive_results, @populated_rful.do_scheduling
+    end
+    
+    test "complex naive scheduling" do
+      @complex_schedule = YAML::load(open("complex_schedule.yml"))
 
-  def test_too_many_requested_resources_gives_error
-    ran_out_of_resources = [{ :task => "NN", :resources => [Resourceful::E_NOT_ENOUGH_RESOURCES] }]
-    rful = Resourceful.new([{ :task => "NN", :time => 1, :resources => 4 }], @resources)
-    assert_equal ran_out_of_resources, rful.do_scheduling
-  end
+      @complex_naive_results = [{ :task=>"A", :resources=>["Eve"] },
+                                { :task=>"B", :resources=>["Bob", "Eve"] },
+                                { :task=>"C", :resources=>["Alice", "Bob", "Eve"] },
+                                { :task=>"D", :resources=>["Alice", "Eve"] },
+                                { :task=>"E", :resources=>["Bob", "Eve"] },
+                                { :task=>"F", :resources=>["Bob"] },
+                                { :task=>"G", :resources=>["Alice", "Bob"] },
+                                { :task=>"H", :resources=>["Alice"] },
+                                { :task=>"I", :resources=>["Alice", "Bob", "Eve"] }]
 
-  def test_naive_scheduling
-    assert_equal @naive_results, @populated_rful.do_scheduling
-  end
+      @populated_rful.schedule = @complex_schedule
 
-  def test_complex_naive_scheduling
-    @populated_rful.schedule = @complex_schedule
-    assert_equal @complex_naive_results, @populated_rful.do_scheduling
+      assert_equal @complex_naive_results, @populated_rful.do_scheduling
+    end
+
+    test "too many requested resources gives error" do
+      rful = Resourceful::Solver.new([{ :task => "NN", :time => 1, :resources => 4 }], @resources)
+      assert_raise Resourceful::NotEnoughResourcesError do
+        rful.do_scheduling
+      end
+    end
+
   end
 end
 
