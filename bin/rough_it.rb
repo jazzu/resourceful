d9dc41b4ea3ce9c6a68b5cb14daa8017b2f6f8a8
Jaakko Vallo <jaakko.vallo@iki.fi>
2010-09-22 18:13:56 +0300

    Adapted to the new code

--- bin/rough_it.rb
+++ bin/rough_it.rb
@@ -5,15 +5,16 @@ require 'pp'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'resourceful')
 
 resources = [
-             { :name => "X", :time_worked => 0, :last_worked => "" },
-             { :name => "Y", :time_worked => 0, :last_worked => "" },
-             { :name => "Z", :time_worked => 0, :last_worked => "" }
+             { :name => "X" },
+             { :name => "Y" },
+             { :name => "Z" }
             ]
 
 complex_schedule = YAML::load(open(File.join(File.dirname(__FILE__), '..', 'test', 'complex_schedule.yml')))
-r = Resourceful.new(complex_schedule, resources)
+r = Resourceful::Solver.new(complex_schedule, resources)
 
-pp r.do_scheduling.sort { |a,b| a[:task] <=> b[:task] }
+pp r.do_scheduling
 
 pp resources
 pp complex_schedule
+
