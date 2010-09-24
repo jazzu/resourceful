c98aa4a2147ef6d0b7bbc0cadf64b30a678e554b
Jaakko Vallo <jaakko.vallo@iki.fi>
2010-09-24 23:54:59 +0300

    Added Rakefile and renamed stuff for clarity

--- lib/resourceful.rb
+++ lib/resourceful.rb
@@ -31,34 +31,34 @@ module Resourceful
       src_schedule = @schedule.sort { |a,b| b[:time] <=> a[:time] }
 
       src_schedule.each do |task|
-        if task[:resources] > @resources.size
-          r_error = NotEnoughResourcesError.new(task[:task], task[:resources], @resources.size)
-          raise r_error, sprintf(E_NOT_ENOUGH_RESOURCES, task[:task], task[:resources], @resources.size) , caller
+        if task[:resources_required] > @resources.size
+          r_error = NotEnoughResourcesError.new(task[:task_name], task[:resources_required], @resources.size)
+          raise r_error, sprintf(E_NOT_ENOUGH_RESOURCES, task[:task_name], task[:resources_required], @resources.size), caller
         else
           workers = []
-          task[:resources].times { workers << least_worked(task[:task], task[:time]) }
+          task[:resources_required].times { workers << least_worked(task[:task_name], task[:time]) }
           workers.sort!
 
-          result_work_schedule[original_position(task[:task])] = { :task => task[:task], :resources => workers }
+          result_work_schedule[original_position(task[:task_name])] = { :task_name => task[:task_name], :resources_allocated => workers }
         end
       end
 
       result_work_schedule
     end
 
-    def original_position(task)
+    def original_position(task_name)
       @schedule.each_index do |i|
-        return i if @schedule[i][:task] == task
+        return i if @schedule[i][:task_name] == task_name
       end
     end
 
   end
 
   class NotEnoughResourcesError < StandardError
-    attr_reader :task, :resources_required, :resources_available
+    attr_reader :task_name, :resources_required, :resources_available
 
     def initialize(failed_task, resources_required, resources_available)
-      @task = failed_task
+      @task_name = failed_task
       @resources_required = resources_required
       @resources_available = resources_available
     end
--- test/test_resourceful.rb
+++ test/test_resourceful.rb
@@ -2,14 +2,14 @@
 require 'rubygems'
 require 'yaml'
 require 'contest'
-require File.join(File.dirname(__FILE__), '..', 'lib', 'resourceful')
+require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'lib', 'resourceful')
 
 class ResourcefulTest < Test::Unit::TestCase
 
   setup do
-    @schedule = [{ :task => "Restroom cleaning", :time => 1, :resources => 2 },
-                 { :task => "Flyer distribution", :time => 2, :resources => 3 },
-                 { :task => "Tour of the venue", :time => 3, :resources => 1 }]
+    @schedule = [{ :task_name => "Restroom cleaning", :time => 1, :resources_required => 2 },
+                 { :task_name => "Flyer distribution", :time => 2, :resources_required => 3 },
+                 { :task_name => "Tour of the venue", :time => 3, :resources_required => 1 }]
 
     @resources = [{ :name => "Alice" },
                   { :name => "Bob" },
@@ -43,32 +43,31 @@ class ResourcefulTest < Test::Unit::TestCase
     end
 
     test "naive scheduling" do
-      @naive_results = [{ :task => "Restroom cleaning", :resources => ["Bob", "Eve"] },
-                        { :task => "Flyer distribution", :resources => ["Alice", "Bob", "Eve"] },
-                        { :task => "Tour of the venue", :resources => ["Alice"] }]
+      @naive_results = [{ :task_name => "Restroom cleaning", :resources_allocated => ["Bob", "Eve"] },
+                        { :task_name => "Flyer distribution", :resources_allocated => ["Alice", "Bob", "Eve"] },
+                        { :task_name => "Tour of the venue", :resources_allocated => ["Alice"] }]
       assert_equal @naive_results, @populated_rful.do_scheduling
     end
     
     test "complex naive scheduling" do
-      @complex_schedule = YAML::load(open("complex_schedule.yml"))
+      @complex_schedule = YAML::load(open( File.join(File.dirname(File.expand_path(__FILE__)), "complex_schedule.yml")))
 
-      @complex_naive_results = [{ :task=>"A", :resources=>["Eve"] },
-                                { :task=>"B", :resources=>["Bob", "Eve"] },
-                                { :task=>"C", :resources=>["Alice", "Bob", "Eve"] },
-                                { :task=>"D", :resources=>["Alice", "Eve"] },
-                                { :task=>"E", :resources=>["Bob", "Eve"] },
-                                { :task=>"F", :resources=>["Bob"] },
-                                { :task=>"G", :resources=>["Alice", "Bob"] },
-                                { :task=>"H", :resources=>["Alice"] },
-                                { :task=>"I", :resources=>["Alice", "Bob", "Eve"] }]
+      @complex_naive_results = [{ :task_name=>"A", :resources_allocated=>["Eve"] },
+                                { :task_name=>"B", :resources_allocated=>["Bob", "Eve"] },
+                                { :task_name=>"C", :resources_allocated=>["Alice", "Bob", "Eve"] },
+                                { :task_name=>"D", :resources_allocated=>["Alice", "Eve"] },
+                                { :task_name=>"E", :resources_allocated=>["Bob", "Eve"] },
+                                { :task_name=>"F", :resources_allocated=>["Bob"] },
+                                { :task_name=>"G", :resources_allocated=>["Alice", "Bob"] },
+                                { :task_name=>"H", :resources_allocated=>["Alice"] },
+                                { :task_name=>"I", :resources_allocated=>["Alice", "Bob", "Eve"] }]
 
       @populated_rful.schedule = @complex_schedule
-
       assert_equal @complex_naive_results, @populated_rful.do_scheduling
     end
 
     test "too many requested resources gives error" do
-      rful = Resourceful::Solver.new([{ :task => "NN", :time => 1, :resources => 4 }], @resources)
+      rful = Resourceful::Solver.new([{ :task_name => "NN", :time => 1, :resources_required => 4 }], @resources)
       assert_raise Resourceful::NotEnoughResourcesError do
         rful.do_scheduling
       end
